87e21ff2d8331f4cd5b4eb3ca78a1988b81fa306
Julio Vázquez <julio.vazquez@getpulpo.com>
2024-07-10 11:22:05 -0600

    fix: Copy files on respective folders

--- documents_upload/src/document.py
+++ documents_upload/src/document.py
@@ -19,10 +19,13 @@ class Document:
 
     def is_valid_plate(self):
         plate = clean_plate(self.get_plate())
-        return plate.isalnum()
+        return plate and plate.isalnum()
 
 def clean_plate(plate):
-    return plate.replace("-", "").replace(" ", "").upper()
+    cleaned_plate = plate.replace("-", "").replace(" ", "").upper()
+    if not cleaned_plate:
+        return "NO PLATE FOR FILE"
+    return cleaned_plate
 
 def get_documents_from_files(path):
     documents = []
@@ -40,51 +43,49 @@ def print_document_info(documents):
         print()
 
 def create_and_move_documents(documents, base_path):
+    invalid_folder_path = os.path.join(base_path, 'invalid-documents')
+    
+    if not os.path.exists(invalid_folder_path):
+        os.makedirs(invalid_folder_path)
+    
     for doc in documents:
+        src = os.path.join(base_path, doc.name)
+        plate = clean_plate(doc.get_plate())
+        
+        print(f"Processing {doc.name}: Plate = {plate}, is_valid = {doc.is_valid_plate()}")
+        
         if doc.is_valid_plate():
-            plate = clean_plate(doc.get_plate())
             folder_path = os.path.join(base_path, plate)
             
-            # Delete all files in the folder
-            if os.path.exists(folder_path):
-                for filename in os.listdir(folder_path):
-                    file_path = os.path.join(folder_path, filename)
-                    if os.path.isfile(file_path):
-                        os.remove(file_path)
-            else:
-                # Create the folder if it doesn't exist
+            if not os.path.exists(folder_path):
                 os.makedirs(folder_path)
             
-            # Move the document to the folder
-            src = os.path.join(base_path, doc.name)
             dst = os.path.join(folder_path, doc.name)
-            shutil.copy(src, dst)
-            print(f"Copy {doc.name} to {folder_path}")
+            
+            if os.path.exists(src):
+                shutil.copy(src, dst)
+                print(f"Copied {doc.name} to {folder_path}")
+            else:
+                print(f"Source file {src} does not exist")
+        else:
+            dst = os.path.join(invalid_folder_path, doc.name)
+            
+            if os.path.exists(src):
+                shutil.copy(src, dst)
+                print(f"Copied {doc.name} to {invalid_folder_path}")
+            else:
+                print(f"Source file {src} does not exist")
 
-
-# Define la ruta del directorio
-directory_path = 'files/'
-
-# Obtén los documentos del directorio
-documents = get_documents_from_files(directory_path)
-
-# Arreglos para almacenar documentos válidos e inválidos
-valid_documents = []
-invalid_documents = []
-
-# Separa los documentos en válidos e inválidos
-for doc in documents:
-    if doc.is_valid_plate():
-        valid_documents.append(doc)
-    else:
-        invalid_documents.append(doc)
-
-# Imprime la información de todos los documentos
-print("VALID DOCUMENTS:")
-print_document_info(valid_documents)
-
-print("INVALID DOCUMENTS:")
-print_document_info(invalid_documents)
-
-# Crea carpetas para los documentos válidos y mueve los documentos a las carpetas correspondientes
-create_and_move_documents(valid_documents, directory_path)
+def get_plate_by_folder_name(base_path):
+    plates = []
+    
+    for folder_name in os.listdir(base_path):
+        folder_path = os.path.join(base_path, folder_name)
+        
+        if os.path.isdir(folder_path):
+            if (folder_name == 'invalid-documents'):
+                continue
+            plate = folder_name.split('_')[0]
+            plates.append(plate)
+    
+    return plates
