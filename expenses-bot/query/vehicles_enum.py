6b9fa47406e101e5762f1fba2079cc1c6ffb02c1
Julio Vázquez <julio.vazquez@ciencias.unam.mx>
2025-01-07 14:57:07 -0600

    Delete wrong name file

--- expenses-bot/query/vehicles_enum.py
+++
@@ -1,9 +0,0 @@
-# Enumeración usando registration_number_v2 y id en la tabla vehicles
-VEHICLES = {
-    "2661LVR": 3029811,
-    "5603MTV": 3029812,
-    "5448MJD": 3029813,
-    "7824KLB": 3029814,
-    "1205XCV": 3029815,
-    "9032PLQ": 3029816,
-}
